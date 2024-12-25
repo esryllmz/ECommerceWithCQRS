be0afc747421bdc390c7be18729db4e51bf8e164
esryllmz <y.esra2794@gmail.com>
2024-12-25 04:16:19 +0300

    25.12

---
+++ src/projects/ECommerce.Application/Features/Categories/Constants/CategoryMessages.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ECommerce.Application.Features.Categories.Constants;
+
+public static class CategoryMessages
+{
+    public const string CategoryNotFoundMessage = "Category Not Found.";
+
+    public static string? CategoryNameMustBeUniqueMessage = "Category Name Must Be Unique!";
+    
+    public const string CategoryNameMustBeMinimumThreeLength= "Category Name Must Be Contain 3 Minimum Characters!";
+}
